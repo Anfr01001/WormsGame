f9fc89daa20ffd94d8128d876339b6ce18404149
André Franzén <anfr01001@utb.vaxjo.se>
2019-09-22 22:42:30 +0200

    Google forms programm för att skapa kator

---
+++ Picture to array/Picture to array/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Picture_to_array
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Startbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Startbtn
+            // 
+            this.Startbtn.Location = new System.Drawing.Point(6, 67);
+            this.Startbtn.Name = "Startbtn";
+            this.Startbtn.Size = new System.Drawing.Size(75, 394);
+            this.Startbtn.TabIndex = 0;
+            this.Startbtn.Text = "Start";
+            this.Startbtn.UseVisualStyleBackColor = true;
+            this.Startbtn.Click += new System.EventHandler(this.Startbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(87, 2);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.OutputBox.Size = new System.Drawing.Size(754, 460);
+            this.OutputBox.TabIndex = 3;
+            this.OutputBox.Text = "";
+            this.OutputBox.TextChanged += new System.EventHandler(this.OutputBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 474);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Startbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Startbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox OutputBox;
+    }
+}
+
---
+++ Picture to array/Picture to array/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_to_array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //pictureBox1.Image =
+        }
+
+        
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = openFileDialog.FileName;
+            }
+
+        }
+
+        private void Startbtn_Click(object sender, EventArgs e)
+        {
+            OutputBox.Text = "";
+
+            Bitmap img = new Bitmap(pictureBox1.ImageLocation);
+            for (int i = 0; i < img.Height; i++)
+            {
+                OutputBox.Text = OutputBox.Text + "{";
+                for (int j = 0; j < img.Width; j++)
+                {
+                    Color pixel = img.GetPixel(j, i);
+
+                    if (pixel.R == 255 && pixel.G == 255 && pixel.B == 255)
+                    {
+                        OutputBox.Text = OutputBox.Text + "0,";
+                    } else
+                    {
+                        OutputBox.Text = OutputBox.Text + "1,";
+                    }
+                }
+
+                OutputBox.Text = OutputBox.Text.Remove(OutputBox.Text.Length - 1);
+                OutputBox.Text = OutputBox.Text + "},";
+            }
+        }
+
+        private void OutputBox_TextChanged(object sender, EventArgs e)
+        {
+           // OutputBox.Text = "{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },";
+        }
+    }
+}
